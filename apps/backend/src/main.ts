92e12c734af65a44f818c3e4c678387fdb0e1c7b
Rifqi <vampvarg@live.com>
2026-06-24 18:51:23 +0700

    increase bodyParser json limit to 10mb to support base64 avatars

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -25,8 +25,8 @@ if (fs.existsSync(srcAssetsDir)) {
   app.use(express.static(srcAssetsDir));
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(routes);
 
 app.get('/', (req: express.Request, res: express.Response) => {
